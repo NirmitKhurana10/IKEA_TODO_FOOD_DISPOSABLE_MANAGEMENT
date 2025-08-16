8d291983c2180f510848c056cf11d3d5333c0f6e
NirmitKhrurana10 <nkhurana1010@gmail.com>
2025-08-16 14:46:12 -0400

    fixed date error in json by addding isoformat

--- scripts/disposable_logger.py
+++ scripts/disposable_logger.py
@@ -25,13 +25,12 @@ yesterday = (datetime.now(tz) - timedelta(days=1)).date()
 rows_to_upsert = []
 for entry in usage:
     rows_to_upsert.append({
-        "log_date": yesterday,
-        "disposable_id": entry["disposable_id"],
-        "quantity_used": entry["quantity_used"],
-        "disposable_name": entry["disposable_name"]
+        "log_date": yesterday.isoformat(),   # fix JSON serialization
+        "disposable_id": entry.get("disposable_id"),
+        "quantity_used": entry.get("quantity_used", 0),
+        "disposable_name": entry.get("disposable_name")
     })
-
-supabase.table("disposable_usage_log").upsert(rows_to_upsert).execute()
+supabase.table("disposable_usage_log").upsert(rows_to_upsert, on_conflict="dipsosable_id, log_date").execute()
     
 # Executing SQL Stored procedure to deduct stock
     
