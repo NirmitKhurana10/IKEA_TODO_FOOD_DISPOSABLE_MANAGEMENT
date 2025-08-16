4a1ab21dd25ee4826cb6e358e521fe97aabd9cd0
NirmitKhrurana10 <nkhurana1010@gmail.com>
2025-08-16 14:13:00 -0400

    added python script to ensure executon of procedure refresh_disposable_daily_usage_view

---
+++ scripts/refresh_daily_disposable_usage_view.py
@@ -0,0 +1,16 @@
+from supabase import create_client
+from dotenv import load_dotenv
+import os
+import time
+
+load_dotenv()
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+supabase = create_client(url, key)
+
+time.sleep(60)
+try:
+    supabase.rpc("refresh_disposable_daily_usage_view").execute()
+except Exception as e:
+    print("Error occured while executing - refresh_daily_disposable_usage_view.py:16")
